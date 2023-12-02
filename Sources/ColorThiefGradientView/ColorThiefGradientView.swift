740dc69790c642f703fde77c41e3c66a1d530747
yukiny0811 <yukiny0811@gmail.com>
2023-12-02 12:50:43 +0900

    aa

--- Sources/ColorThiefGradientView/ColorThiefGradientView.swift
+++ Sources/ColorThiefGradientView/ColorThiefGradientView.swift
@@ -1,2 +1,41 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+import ColorThiefSwift
+
+public struct GradientImageView: View {
+    
+    var color1: UIColor = .black
+    var color2: UIColor = .black
+    
+    public init(image: UIImage) {
+        guard let colors = ColorThief
+            .getPalette(
+                from: image,
+                colorCount: 2,
+                quality: 1,
+                ignoreWhite: true
+            ) else {
+            return
+        }
+        guard colors.count == 2 else {
+            return
+        }
+        color1 = colors[0].makeUIColor()
+        color2 = colors[1].makeUIColor()
+    }
+    
+    public var body: some View {
+        LinearGradient(
+            gradient: Gradient(
+                colors: [
+                    Color(cgColor: color1.cgColor),
+                    Color(cgColor: color2.cgColor)
+                ]
+            ),
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing
+        )
+    }
+}
